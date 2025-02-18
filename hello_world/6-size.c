5c86de146866a8bba0b1151be583572cf6396886
LuvliNay <nayshka1206@gmail.com>
2025-02-18 11:23:23 -0400

    Size is not grandeur

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,12 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-	Size of a char: 1 byte(s)
-        Size of an int: 4 byte(s)
-        Size of a long int: 4 byte(s)
-        Size of a long long int: 8 byte(s)
-        Size of a float: 4 byte(s)
-
-        return (0);
-}	
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
