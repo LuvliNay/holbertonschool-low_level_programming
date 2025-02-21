21b0c6a3b1e92f07a09310fd381596c090675bd2
LuvliNay <nayshka1206@gmail.com>
2025-02-21 11:09:35 -0400

    Patience...is important

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,16 +10,10 @@ int main(void)
 	int num;
 
 	for (num = 10; num <= 9; num++)
-	{
-		putchar(num + '0');
-
 		if (num != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-
+			putchar(num + '0');
+	putchar(',');
+	putchar(' ');
 	putchar('\n');
 
 	return (0);
