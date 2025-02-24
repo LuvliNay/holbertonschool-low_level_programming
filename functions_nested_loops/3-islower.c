bf98039cdc685a8f5141169f4b40683bb29b5dc7
LuvliNay <nayshka1206@gmail.com>
2025-02-24 14:48:19 -0400

    Check lowercase character

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _islower - checks for lowercase character.
+ * @c: the character to check.
+ *
+ * Return: SUCCESS (1)
+ */
 int _islower(int c)
 {
-	if (c <= 'a' && c <= 'z')
-	{
+	if (c >= 'a' && c <= 'z')
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
-}
-
-int main()
-{
-	char testChar1 = 'g';
-	char testChar2 = 'G';
-
-	printf("%c is lowercase? %d\n", testChar1, _islower(testChar1));
-	printf("%c is lowercase? %d\n", testChar2, _islower(testChar2));
-
-	return (0);
 }
