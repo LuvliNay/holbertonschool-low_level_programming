16df0d2ca3ad59bbea325288abb6c11557ffe71c
LuvliNay <nayshka1206@gmail.com>
2025-02-24 21:26:18 -0400

    FINALLYgit add .!

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -9,8 +9,8 @@
  */
 int _isalpha(int c)
 {
-        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-                return (1);
-        else
-                return (0);
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
 }
