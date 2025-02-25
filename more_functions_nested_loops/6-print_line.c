6ac78a9eb8113139d4e7f5fb78b03d468a696c11
LuvliNay <nayshka1206@gmail.com>
2025-02-25 12:50:55 -0400

    task 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,17 +3,18 @@
 /**
  * print_line - draws a straight line in the terminal.
  * @n: variable given.
- *
  * Return: Always 0.
  */
 void print_line(int n)
 {
-	int line = '_';
-
-	while (n >= 0)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		_putchar(line);
-	n--;
+		int i;
+
+		for (i = 0; i < n; i++)
+			_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
