3f6939eaa243506ea92bd88dadd21c7c98760dff
LuvliNay <nayshka1206@gmail.com>
2025-02-24 23:39:11 -0400

    adding two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers and returns the result.
+ * @num1: is the variable given.
+ * @num2: is the variable given.
+ * Return: SUCCESS (0)
+ */
+
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
