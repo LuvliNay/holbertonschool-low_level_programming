4d381e943f15e4db283038da098ede270c111c09
LuvliNay <nayshka1206@gmail.com>
2025-02-28 19:40:11 -0400

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 char *_strcat(char *dest, char *src);
+int _strlen(char *s)
 char *_strncat(char *dest, char *src, int n);
+char *_strcpy(char *dest, char *src);
