17bf4d5a507958d9644349a55bcd6dd789cfab1c
LuvliNay <nayshka1206@gmail.com>
2025-02-28 21:32:45 -0400

    task 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * reverse_array - prints an array in reverse.
+ *
+ * @a: pointer.
+ *
+ * @n: pointer.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}
