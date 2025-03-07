5615e7ad686653a2236f3f3774819a26dd09a516
LuvliNay <nayshka1206@gmail.com>
2025-03-07 09:52:52 -0400

    0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - fill memory.
+ *
+ * @s: pointer.
+ *
+ * @b: variable.
+ *
+ * @n: unsigned integer.
+ *
+ * Return: value 0.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
